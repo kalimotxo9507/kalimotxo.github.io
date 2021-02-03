9926df4a865e3720f72cde9fd8c13566a0f8af88
kawarai.tao20 <kawarai.tao20@fixer.co.jp>
2021-02-04 08:29:34 +0900

    ✏️ 20210204-article

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,22 @@
 <template lang="md">
   ## このサイトについて
+
+kalimotxoの個人ブログ
+
+  ## About kalimotxo
+
+engineer
+
+### hobby
+
+プログラミング、ジャグリング、ジョギング、筋トレ、エレクトーン、
+
+### skill
+
+Python, C, C#, Shell Script, JavaScript, TypeScript, CSS, HTML
+
+AZ-900, AZ-500, PL-900
+
 </template>
 
 <script>
